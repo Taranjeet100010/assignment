6d88438ad8d524ce25c12c8f1350b3ce32060266
Taranjeet100010 <taranjeet@chicmic.co.in>
2021-10-21 19:13:01 +0530

    ghg

--- midterm.js
+++ midterm.js
@@ -1,88 +1,91 @@
-let vehicles=[
+let vehicles = [
     {
-        id:1,
-        productName:"Cars",
+        id: 1,
+        productName: "Cars",
         inventory: 15,
         unitsSold: 30,
-        brand:["Tesla","Daimler","Toyota","Ferrari"]
+        brand: ["Tesla", "Daimler", "Toyota", "Ferrari"]
     },
     {
-        id:2,
-        productName:"Cars",
+        id: 2,
+        productName: "Cars",
         inventory: 20,
         unitsSold: 50,
-        brand:["Ducati","Ferrari","BMW","Hyundai"]
+        brand: ["Ducati", "Ferrari", "BMW", "Hyundai"]
     },
     {
-        id:3,
-        productName:"Cars",
+        id: 3,
+        productName: "Cars",
         inventory: 12,
         unitsSold: 40,
-        brand:["Benz","Tesla"]
+        brand: ["Benz", "Tesla"]
     },
-    {        
-        id:4,
-        productName:"Cars",
+    {
+        id: 4,
+        productName: "Cars",
         inventory: 18,
         unitsSold: 60,
-        brand:["BMW","Mercedes","Otdi"]
+        brand: ["BMW", "Mercedes", "Otdi"]
     },
     {
-        id:5,
-        productName:"Cars",
+        id: 5,
+        productName: "Cars",
         inventory: 12,
         unitsSold: 20,
-        brand:["Kia","Duster","Ferrari"]
+        brand: ["Kia", "Duster", "Ferrari"]
     },
     {
-        id:6,
-        productName:"Cars",
+        id: 6,
+        productName: "Cars",
         inventory: 15,
         unitsSold: 35,
-        brand:["Tata","Volvo","Benz"]
+        brand: ["Tata", "Volvo", "Benz"]
     },
     {
-        id:7,
-        productName:"Cars",
+        id: 7,
+        productName: "Cars",
         inventory: 16,
         unitsSold: 50,
-        brand:["Tata","Benz"]
+        brand: ["Tata", "Benz"]
     },
     {
-        id:8,
-        productName:"Cars",
+        id: 8,
+        productName: "Cars",
         inventory: 18,
         unitsSold: 26,
-        brand:["Nissan","Honda","Toyota"]
+        brand: ["Nissan", "Honda", "Toyota"]
     },
     {
-        id:9,
-        productName:"Cars",
+        id: 9,
+        productName: "Cars",
         inventory: 19,
         unitsSold: 32,
-        brand:["Honda","Nissan","Volkswagen"]
+        brand: ["Honda", "Nissan", "Volkswagen"]
     },
     {
-        id:10,
-        productName:"Cars",
+        id: 10,
+        productName: "Cars",
         inventory: 11,
         unitsSold: 24,
-        brand:["Volkswagen","Tata","Toyota"]
+        brand: ["Volkswagen", "Tata", "Toyota"]
     }
 ]
 
-function calculateInventoryCost(obj){
+function calculateInventoryCost(obj) {
+    obj.brand.forEach(brand => {
+        
+        vehicles.forEach(vehicle => {
+            if (vehicle.brand.includes(brand)) {
+                console.log("vhjhjvbhj")
+            }
+        });
 
-obj.brand.forEach(brand=>{
-    vehicles.forEach(vehicle =>{
-        if(vehicle.brand.includes(brand)){
-            
-        }
-    });
-
+    })
 }
 
-
-)
-   
-}
+calculateInventoryCost({
+    productName: "Cars",
+        inventory: 19,
+        unitsSold: 32,
+        brand: ["Honda", "Nissan", "Volkswagen"]
+});
